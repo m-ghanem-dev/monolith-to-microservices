0320ee5286805eafd451874450773cb80cdc2cd3
m-ghanem-dev <mohamedgh4444@gmail.com>
2026-01-04 22:40:25 +0100

    jwt secret

--- udagram-user/src/server.ts
+++ udagram-user/src/server.ts
@@ -5,7 +5,6 @@ import {sequelize} from './sequelize';
 import {IndexRouter} from './controllers/v0/index.router';
 
 import bodyParser from 'body-parser';
-import {config} from './config/config';
 import {V0_USER_MODELS} from './controllers/v0/model.index';
 
 
@@ -44,7 +43,7 @@ import {V0_USER_MODELS} from './controllers/v0/model.index';
 
   // Start the Server
   app.listen( port, () => {
-    console.log( `server running ${config.url}` );
+    console.log( `server running ...` );
     console.log( `press CTRL+C to stop server` );
   } );
 })();
